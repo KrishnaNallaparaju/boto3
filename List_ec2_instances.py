180587ed85fcbd3a9fafb571e0695b278779f02b
Krishna Raviteja Varma <nkrtvarma1995@gmail.com>
2020-12-07 09:05:45 +0530

    Adding the EC2 code

---
+++ List_ec2_instances.py
@@ -0,0 +1,10 @@
+import boto3
+
+#Set the Session object!
+sess_obj=boto3.Session(profile_name='cloud-user', region_name='us-east-1')
+
+res_obj=sess_obj.resource('ec2')
+
+for instance in res_obj.instances.all():
+    print(instance)
+
